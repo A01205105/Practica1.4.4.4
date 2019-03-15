660eaf3ad2f2c8c915c599c3f9b4ce925b8eaf75
Valdemar <valde1hs@hotmail.com>
2019-03-15 11:56:50 -0600

    Main

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdout.h>
+
+#define VARIABLE 0
+#define VARIABLE 1
+#define VARIABLE 2
+#define VARIABLE 3
+#define VARIABLE 4
+#define VARIABLE 5
+
+int suma(int a, int b);
+
+int main (void)
+{
+	printf("Hola");
+	return 0;
+}
+
+/* Control de Cambios
+01 | Se agregó una librería			| Valdemar Hernández 15/03/19
+02 | Se agregó una nueva variable	| Valdemar Hernández 15/03/19
+03 | Se agregó una tercera variable	| Valdemar Hernández 15/03/19
+*/
